176495a34e39de210090675f92569cdcf7140c34
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:04:01 +0100

    Add `isFunction` tests and improve function documentation

--- src/lang/isFunction.ts
+++ src/lang/isFunction.ts
@@ -1,4 +1,13 @@
-export const isFunction = (
-  value: unknown,
-): value is (..._args: unknown[]) => unknown => typeof value === "function";
-
+/**
+ * Checks if a value is a function.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a function.
+ *
+ * @example
+ * isFunction(() => {}); // true
+ * isFunction(123); // false
+ */
+export const isFunction = (value: unknown): value is (..._args: unknown[]) => unknown => {
+  return typeof value === "function";
+};

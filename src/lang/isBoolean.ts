a9a2c6151eb04016bacb7727b7991dd2026de1cf
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 21:53:57 +0100

    Add `isBoolean` tests and improve function documentation

--- src/lang/isBoolean.ts
+++ src/lang/isBoolean.ts
@@ -1,2 +1,13 @@
-export const isBoolean = (value: unknown): value is boolean => typeof value === "boolean";
-
+/**
+ * Checks if a value is a boolean.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a boolean.
+ *
+ * @example
+ * isBoolean(true); // true
+ * isBoolean("true"); // false
+ */
+export const isBoolean = (value: unknown): value is boolean => {
+  return typeof value === "boolean";
+};

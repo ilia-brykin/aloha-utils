3bba40452e0c3b2c25aaa2cb45e9f8851aa72eb1
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:09:20 +0100

    Add `isNil` tests and improve function documentation

--- src/lang/isNil.ts
+++ src/lang/isNil.ts
@@ -1,2 +1,13 @@
-export const isNil = (value: unknown): value is null | undefined => value === null || value === undefined;
-
+/**
+ * Checks if a value is null or undefined.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is null or undefined.
+ *
+ * @example
+ * isNil(null); // true
+ * isNil(0); // false
+ */
+export const isNil = (value: unknown): value is null | undefined => {
+  return value === null || value === undefined;
+};

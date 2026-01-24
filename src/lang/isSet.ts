0ea2020a7c1e1d9b307ed26b0853230982440209
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:16:15 +0100

    Add `isSet` tests and improve function documentation

--- src/lang/isSet.ts
+++ src/lang/isSet.ts
@@ -2,5 +2,16 @@ import {
   toString,
 } from "./_shared.js";
 
-export const isSet = (value: unknown): value is Set<unknown> => toString.call(value) === "[object Set]";
-
+/**
+ * Checks if a value is a Set.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a Set.
+ *
+ * @example
+ * isSet(new Set()); // true
+ * isSet(new Map()); // false
+ */
+export const isSet = (value: unknown): value is Set<unknown> => {
+  return toString.call(value) === "[object Set]";
+};

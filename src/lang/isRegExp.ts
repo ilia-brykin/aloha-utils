25d4573f0f9be167a880354fb22fcb889c0bc699
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:15:18 +0100

    Add `isRegExp` tests and improve function documentation

--- src/lang/isRegExp.ts
+++ src/lang/isRegExp.ts
@@ -2,5 +2,16 @@ import {
   toString,
 } from "./_shared.js";
 
-export const isRegExp = (value: unknown): value is RegExp => toString.call(value) === "[object RegExp]";
-
+/**
+ * Checks if a value is a RegExp.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a RegExp.
+ *
+ * @example
+ * isRegExp(/test/); // true
+ * isRegExp("test"); // false
+ */
+export const isRegExp = (value: unknown): value is RegExp => {
+  return toString.call(value) === "[object RegExp]";
+};

aec5e89b8f07320da6a6b66b7350e80aaa10346c
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:01:40 +0100

    Add `isFinite` tests and improve function documentation

--- src/lang/isFinite.ts
+++ src/lang/isFinite.ts
@@ -1,2 +1,13 @@
-export const isFinite = (value: unknown): value is number => typeof value === "number" && Number.isFinite(value);
-
+/**
+ * Checks if a value is a finite number.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a finite number.
+ *
+ * @example
+ * isFinite(10); // true
+ * isFinite(Infinity); // false
+ */
+export const isFinite = (value: unknown): value is number => {
+  return typeof value === "number" && Number.isFinite(value);
+};

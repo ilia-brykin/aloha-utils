b97b03fcc516ab9d77fd6e88499dfd357de07775
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:13:09 +0100

    Add `isObjectLike` tests and improve function documentation

--- src/lang/isObjectLike.ts
+++ src/lang/isObjectLike.ts
@@ -1,2 +1,13 @@
-export const isObjectLike = (value: unknown): value is object => value !== null && typeof value === "object";
-
+/**
+ * Checks if a value is object-like (non-null and typeof "object").
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is object-like.
+ *
+ * @example
+ * isObjectLike({}); // true
+ * isObjectLike(null); // false
+ */
+export const isObjectLike = (value: unknown): value is object => {
+  return value !== null && typeof value === "object";
+};

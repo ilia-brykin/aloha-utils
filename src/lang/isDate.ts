b70235500484470294a4a3aca7866927757337a5
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 21:56:52 +0100

    Add `isDate` tests and improve function documentation

--- src/lang/isDate.ts
+++ src/lang/isDate.ts
@@ -2,5 +2,16 @@ import {
   toString,
 } from "./_shared.js";
 
-export const isDate = (value: unknown): value is Date => toString.call(value) === "[object Date]";
-
+/**
+ * Checks if a value is a Date object.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a Date.
+ *
+ * @example
+ * isDate(new Date()); // true
+ * isDate(Date.now()); // false
+ */
+export const isDate = (value: unknown): value is Date => {
+  return toString.call(value) === "[object Date]";
+};

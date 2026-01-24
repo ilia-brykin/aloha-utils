1210f398516ad89f348bfeddeb02e22bfdf6dcb0
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:05:41 +0100

    Add `isInteger` tests and improve function documentation

--- src/lang/isInteger.ts
+++ src/lang/isInteger.ts
@@ -1,2 +1,13 @@
-export const isInteger = (value: unknown): value is number => typeof value === "number" && Number.isInteger(value);
-
+/**
+ * Checks if a value is an integer.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is an integer.
+ *
+ * @example
+ * isInteger(10); // true
+ * isInteger(10.5); // false
+ */
+export const isInteger = (value: unknown): value is number => {
+  return typeof value === "number" && Number.isInteger(value);
+};

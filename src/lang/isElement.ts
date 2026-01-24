6413dd0740c87ff1e5f2c85d6e21cdd91ed642d7
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 21:59:16 +0100

    Add `isElement` tests and improve function documentation

--- src/lang/isElement.ts
+++ src/lang/isElement.ts
@@ -1,3 +1,13 @@
+/**
+ * Checks if a value is a DOM Element.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is an Element.
+ *
+ * @example
+ * isElement(document.createElement("div")); // true
+ * isElement({}); // false
+ */
 export const isElement = (value: unknown): value is Element => {
   if (typeof Element !== "undefined" && value instanceof Element) {
     return true;

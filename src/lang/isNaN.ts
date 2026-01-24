aff9be9712efd002a6aa6cb5990fd0704326fb50
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:08:10 +0100

    Add `isNaN` tests and improve function documentation

--- src/lang/isNaN.ts
+++ src/lang/isNaN.ts
@@ -1,2 +1,13 @@
-export const isNaN = (value: unknown): boolean => Number.isNaN(value);
-
+/**
+ * Checks if a value is NaN.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is NaN.
+ *
+ * @example
+ * isNaN(NaN); // true
+ * isNaN("NaN"); // false
+ */
+export const isNaN = (value: unknown): boolean => {
+  return Number.isNaN(value);
+};

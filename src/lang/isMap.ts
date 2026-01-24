ad323587321daa90ad96e803b6bf7ae5cc4766fb
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:06:59 +0100

    Add `isMap` tests and improve function documentation

--- src/lang/isMap.ts
+++ src/lang/isMap.ts
@@ -2,5 +2,17 @@ import {
   toString,
 } from "./_shared.js";
 
-export const isMap = (value: unknown): value is Map<unknown, unknown> => toString.call(value) === "[object Map]";
+/**
+ * Checks if a value is a Map.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a Map.
+ *
+ * @example
+ * isMap(new Map()); // true
+ * isMap(new Set()); // false
+ */
+export const isMap = (value: unknown): value is Map<unknown, unknown> => {
+  return toString.call(value) === "[object Map]";
+};
 

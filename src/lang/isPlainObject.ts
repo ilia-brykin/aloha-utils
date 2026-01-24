fa627f8d6a4b24f5c350e7dfc494f45c3f9e12f3
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:14:13 +0100

    Add `isPlainObject` tests and improve function documentation

--- src/lang/isPlainObject.ts
+++ src/lang/isPlainObject.ts
@@ -2,12 +2,23 @@ import {
   toString,
 } from "./_shared.js";
 
+/**
+ * Checks if a value is a plain object.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a plain object.
+ *
+ * @example
+ * isPlainObject({}); // true
+ * isPlainObject(new (class Foo {})()); // false
+ */
 export const isPlainObject = (value: unknown): value is Record<string, unknown> => {
   if (toString.call(value) !== "[object Object]") {
     return false;
   }
 
   const proto = Object.getPrototypeOf(value);
+
   return proto === null || proto === Object.prototype;
 };
 

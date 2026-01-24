285a2e6e8bca4cecc1daf81b814e31e03fe74d1a
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:17:04 +0100

    Add `isString` tests and improve function documentation

--- src/lang/isString.ts
+++ src/lang/isString.ts
@@ -1,2 +1,13 @@
-export const isString = (value: unknown): value is string => typeof value === "string";
-
+/**
+ * Checks if a value is a string.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a string.
+ *
+ * @example
+ * isString("hello"); // true
+ * isString(123); // false
+ */
+export const isString = (value: unknown): value is string => {
+  return typeof value === "string";
+};

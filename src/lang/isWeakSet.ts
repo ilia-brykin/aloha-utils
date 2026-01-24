d2bfcb354461f7f81c71d95b24d1c43baac9bc47
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:19:30 +0100

    Add `isWeakSet` tests and improve function documentation

--- src/lang/isWeakSet.ts
+++ src/lang/isWeakSet.ts
@@ -2,5 +2,16 @@ import {
   toString,
 } from "./_shared.js";
 
-export const isWeakSet = (value: unknown): value is WeakSet<object> => toString.call(value) === "[object WeakSet]";
-
+/**
+ * Checks if a value is a WeakSet.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is a WeakSet.
+ *
+ * @example
+ * isWeakSet(new WeakSet()); // true
+ * isWeakSet(new Set()); // false
+ */
+export const isWeakSet = (value: unknown): value is WeakSet<object> => {
+  return toString.call(value) === "[object WeakSet]";
+};

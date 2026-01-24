6368951b2b8aeab25ab455c0d3dc7b6be2ff71fd
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:10:19 +0100

    Add `isNull` tests and improve function documentation

--- src/lang/isNull.ts
+++ src/lang/isNull.ts
@@ -1,2 +1,13 @@
-export const isNull = (value: unknown): value is null => value === null;
-
+/**
+ * Checks if a value is null.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is null.
+ *
+ * @example
+ * isNull(null); // true
+ * isNull(undefined); // false
+ */
+export const isNull = (value: unknown): value is null => {
+  return value === null;
+};

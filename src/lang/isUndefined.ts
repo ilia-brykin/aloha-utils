5300629f0b29718da234f6fe4a02bcb05150e874
ilia.brykin <brykin.ilia@gmail.com>
2026-01-24 22:17:51 +0100

    Add `isUndefined` tests and improve function documentation

--- src/lang/isUndefined.ts
+++ src/lang/isUndefined.ts
@@ -1,2 +1,13 @@
-export const isUndefined = (value: unknown): value is undefined => value === undefined;
-
+/**
+ * Checks if a value is undefined.
+ *
+ * @param {*} value - The value to check.
+ * @return {boolean} True if the value is undefined.
+ *
+ * @example
+ * isUndefined(undefined); // true
+ * isUndefined(null); // false
+ */
+export const isUndefined = (value: unknown): value is undefined => {
+  return value === undefined;
+};
